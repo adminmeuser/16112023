bfa16950f6659769d82f05a96d8e72ccdac594b1
adminmeuser <146311162+adminmeuser@users.noreply.github.com>
2023-11-16 22:07:41 +0530

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,29 @@
+import {NgModule} from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+
+
+
+
+export const routes: Routes = [
+
+{
+    component:HomeComponent,
+    path: ''
+},
+{
+    component:AboutComponent,
+    path:'About'
+},
+{
+    component:ContactComponent,
+    path:'Contact'
+},
+{
+    component:ProductComponent,
+    path:'Product'
+},
+];
